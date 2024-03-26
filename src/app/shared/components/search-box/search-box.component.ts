ebf1a95df3eec1cb5b8ec0c4542f719de8e3af21
Sergio Barreras <sergiobg.isc@gmail.com>
2024-03-26 10:15:12 -0700

    Funcionamiento del SearchBox

--- src/app/shared/components/search-box/search-box.component.ts
+++ src/app/shared/components/search-box/search-box.component.ts
@@ -1,4 +1,11 @@
-import { Component, Input } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  Input,
+  Output,
+  ViewChild,
+} from '@angular/core';
 
 @Component({
   selector: 'shared-search-box',
@@ -7,4 +14,11 @@ import { Component, Input } from '@angular/core';
 export class SearchBoxComponent {
   @Input()
   public placeholder: string = '';
+
+  @Output()
+  public onValue = new EventEmitter<string>();
+
+  emitValue(value: string): void {
+    this.onValue.emit(value);
+  }
 }
