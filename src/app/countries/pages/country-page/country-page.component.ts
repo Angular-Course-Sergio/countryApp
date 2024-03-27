3cb6082b2fdb264e7904313ee08d7c63b5b88385
Sergio Barreras <sergiobg.isc@gmail.com>
2024-03-26 21:29:25 -0700

    Argumentos por URL - Página de País

--- src/app/countries/pages/country-page/country-page.component.ts
+++ src/app/countries/pages/country-page/country-page.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { CountriesService } from '../../services/countries.service';
 
 @Component({
   selector: 'app-country-page',
   templateUrl: './country-page.component.html',
-  styles: ``
+  styles: ``,
 })
-export class CountryPageComponent {
+export class CountryPageComponent implements OnInit {
+  constructor(
+    private countriesService: CountriesService,
+    private activatedRoute: ActivatedRoute
+  ) {}
 
+  ngOnInit(): void {
+    this.activatedRoute.params.subscribe(({ id }) => {
+      this.countriesService
+        .searchCountryByAlphaCode(id)
+        .subscribe((country) => {
+          console.log(country);
+        });
+    });
+  }
 }
