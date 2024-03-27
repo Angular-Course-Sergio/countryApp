e5868ae03d57df09accce6a646ba8d2de2c8678f
Sergio Barreras <sergiobg.isc@gmail.com>
2024-03-27 08:07:17 -0700

    Refactorización del servicio

--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -9,6 +9,10 @@ export class CountriesService {
 
   constructor(private http: HttpClient) {}
 
+  private getCountriesRequest(url: string): Observable<Country[]> {
+    return this.http.get<Country[]>(url).pipe(catchError(() => of([])));
+  }
+
   searchCountryByAlphaCode(code: string): Observable<Country | null> {
     const url = `${this.apiUrl}/alpha/${code}`;
     return this.http.get<Country[]>(url).pipe(
@@ -19,16 +23,16 @@ export class CountriesService {
 
   searchCapital(term: string): Observable<Country[]> {
     const url = `${this.apiUrl}/capital/${term}`;
-    return this.http.get<Country[]>(url).pipe(catchError(() => of([])));
+    return this.getCountriesRequest(url);
   }
 
   searchCountry(name: string): Observable<Country[]> {
     const url = `${this.apiUrl}/name/${name}`;
-    return this.http.get<Country[]>(url).pipe(catchError(() => of([])));
+    return this.getCountriesRequest(url);
   }
 
   searchRegion(region: string): Observable<Country[]> {
     const url = `${this.apiUrl}/region/${region}`;
-    return this.http.get<Country[]>(url).pipe(catchError(() => of([])));
+    return this.getCountriesRequest(url);
   }
 }
