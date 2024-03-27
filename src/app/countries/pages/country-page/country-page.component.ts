862746030983e8375cf5220061ed03d0af2d5bd5
Sergio Barreras <sergiobg.isc@gmail.com>
2024-03-26 21:47:57 -0700

    switchMap - Refactorización

--- src/app/countries/pages/country-page/country-page.component.ts
+++ src/app/countries/pages/country-page/country-page.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { CountriesService } from '../../services/countries.service';
+import { switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-country-page',
@@ -10,16 +11,22 @@ import { CountriesService } from '../../services/countries.service';
 export class CountryPageComponent implements OnInit {
   constructor(
     private countriesService: CountriesService,
-    private activatedRoute: ActivatedRoute
+    private activatedRoute: ActivatedRoute,
+    private router: Router
   ) {}
 
   ngOnInit(): void {
-    this.activatedRoute.params.subscribe(({ id }) => {
-      this.countriesService
-        .searchCountryByAlphaCode(id)
-        .subscribe((country) => {
-          console.log(country);
-        });
-    });
+    this.activatedRoute.params
+      .pipe(
+        switchMap(({ id }) =>
+          this.countriesService.searchCountryByAlphaCode(id)
+        )
+      )
+      .subscribe((country) => {
+        if (!country) return this.router.navigateByUrl('');
+
+        console.log('TENEMOS UN PAÍS');
+        return;
+      });
   }
 }
