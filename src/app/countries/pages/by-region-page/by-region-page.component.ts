7ac0b58ad23e36255fe7476cb1c10dd2c8fe29e5
Sergio Barreras <sergiobg.isc@gmail.com>
2024-03-28 07:31:48 -0700

    Buscar por region - Optimización

--- src/app/countries/pages/by-region-page/by-region-page.component.ts
+++ src/app/countries/pages/by-region-page/by-region-page.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { Country } from '../../interfaces/country';
 import { CountriesService } from '../../services/countries.service';
 
+type Region = 'Africa' | 'America' | 'Asia' | 'Europe' | 'Oceania';
+
 @Component({
   selector: 'app-by-region-page',
   templateUrl: './by-region-page.component.html',
@@ -9,10 +11,14 @@ import { CountriesService } from '../../services/countries.service';
 export class ByRegionPageComponent {
   public countries: Country[] = [];
   public isLoading = false;
+  public regions: Region[] = ['Africa', 'America', 'Asia', 'Europe', 'Oceania'];
+  public selectedRegion?: Region;
 
   constructor(private countriesService: CountriesService) {}
 
-  seachByRegion(term: string): void {
+  seachByRegion(term: Region): void {
+    this.selectedRegion = term;
+
     this.isLoading = true;
     this.countriesService.searchRegion(term).subscribe((countries) => {
       this.countries = countries;
